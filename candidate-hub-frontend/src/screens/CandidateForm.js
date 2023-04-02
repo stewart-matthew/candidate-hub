554d96b99c9dfa3f270ca8ebafc48b2fed5d2e82
malachi-crain <malachicrain@gmail.com>
2023-04-02 00:14:12 -0500

    Lets goooo

--- candidate-hub-frontend/src/screens/CandidateForm.js
+++ candidate-hub-frontend/src/screens/CandidateForm.js
@@ -2,10 +2,19 @@ import { Text, View, StyleSheet, ScrollView, TextInput, React, KeyboardAvoidingV
 import Button from '../components/Button';
 import InputField from '../components/InputField';
 import { styles } from '../styles/styles';
+import { useState } from 'react';
 
 const CandidateFormScreen = ({ route, navigation }) => {
   const uri = route.params;
- // const [text, onChangeText] = React.useState('This is actual text');
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [major, setMajor] = useState('');
+  const [gpa, setGPA] = useState('');
+  const [graduationDate, setGraduationDate] = useState('');
+  const [positionType, setPositionType] = useState('');
+  const [sponsorshipNeeded, setSponsorshipNeeded] = useState('');
 
   return (
     <KeyboardAvoidingView 
@@ -16,43 +25,56 @@ const CandidateFormScreen = ({ route, navigation }) => {
       <ScrollView style={styles.ScrollView}>     
       <View style = {styles.inputField}>     
             <Text style = {styles.inputFieldText}> {"First Name"}{"\n"}</Text>
-            <TextInput style={styles.textInput}  inputMode={"text"}/>
+            <TextInput style={styles.textInput}  inputMode={"text"} onChangeText={(txt) => setFirstName(txt)}/>
           </View>
           <View style = {styles.inputField}>     
             <Text style = {styles.inputFieldText}> {"Last Name"}{"\n"}</Text>
-            <TextInput style={styles.textInput}  inputMode={"text"}/>
+            <TextInput style={styles.textInput}  inputMode={"text"} onChangeText={(txt) => setLastName(txt)}/>
           </View> 
           <View style = {styles.inputField}>     
             <Text style = {styles.inputFieldText}> {"Email Adress"}{"\n"}</Text>
-            <TextInput style={styles.textInput}  inputMode={"email"}/>
+            <TextInput style={styles.textInput}  inputMode={"email"} onChangeText={(txt) => setEmail(txt)}/>
           </View> 
           <View style = {styles.inputField}>     
             <Text style = {styles.inputFieldText}> {"Major"}{"\n"}</Text>
-            <TextInput style={styles.textInput}  inputMode={"text"}/>
+            <TextInput style={styles.textInput}  inputMode={"text"} onChangeText={(txt) => setMajor(txt)}/>
           </View> 
           <View style = {styles.inputField}>     
             <Text style = {styles.inputFieldText}> {"GPA"}{"\n"}</Text>
-            <TextInput style={styles.textInput}  inputMode={"decimal"}/>
+            <TextInput style={styles.textInput}  inputMode={"decimal"}  onChangeText={(txt) => setGPA(txt)}/>
           </View> 
           <View style = {styles.inputField}>     
             <Text style = {styles.inputFieldText}> {"Graduation Date"}{"\n"}</Text>
-            <TextInput style={styles.textInput}  inputMode={"text"}/>
+            <TextInput style={styles.textInput}  inputMode={"text"} onChangeText={(txt) => setGraduationDate(txt)}/>
           </View> 
           <View style = {styles.inputField}>     
             <Text style = {styles.inputFieldText}> {"Position Type"}{"\n"}</Text>
-            <TextInput style={styles.textInput}  inputMode={"text"}/>
+            <TextInput style={styles.textInput}  inputMode={"text"} onChangeText={(txt) => setPositionType(txt)}/>
           </View>   
           <View style = {styles.inputField}>     
             <Text style = {styles.inputFieldText}> {"Sponsership Needed"}{"\n"}</Text>
-            <TextInput style={styles.textInput}  inputMode={"text"}/>
+            <TextInput style={styles.textInput}  inputMode={"text"} onChangeText={(txt) => setSponsorshipNeeded(txt)}/>
             </View>
           <Button 
             onPress={() => { 
-              // get values from input fields and add to data object
-              let data = {
-                
+              if (!firstName) return alert('The "First Name" field is required.')
+              if (!lastName) return alert('The "Last Name" field is required.')
+              if (!email) return alert('The "Email Address" field is required.')
+              if (!major) return alert('The "Major" field is required.')
+              if (!gpa) return alert('The "GPA" field is required.')
+              if (!graduationDate) return alert('The "Graduation Date" field is required.')
+              if (!positionType) return alert('The "Position Type" field is required.')
+              let data = { firstName, lastName, email, major, gpa, graduationDate, positionType, sponsorshipNeeded };
+              data.gpa = Number(gpa);
+              if (sponsorshipNeeded) {
+                data.sponsorshipNeeded = true;
+              } else {
+                data.sponsorshipNeeded = false;
               }
-              data.resumeURI = uri;
+              data.uploadDate = new Date().toString();
+              data.event = 'UA Innovate 2023';
+              if (uri) data.resumeURI = uri;
+              console.log(data)
               // add data to api request
               // if success go to confirmation screen:
               navigation.navigate('ConfirmAddCandidate') 
