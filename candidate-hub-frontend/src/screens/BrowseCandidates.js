63b87374a1f60b0a2f28cfd8c7ae35026e197e6c
malachi-crain <malachicrain@gmail.com>
2023-04-01 23:29:54 -0500

    Sorting

--- candidate-hub-frontend/src/screens/BrowseCandidates.js
+++ candidate-hub-frontend/src/screens/BrowseCandidates.js
@@ -16,14 +16,19 @@ const BrowseCandidatesScreen = ({ navigation }) => {
         setCandidates(res.data);
       })
       .catch((err) => {
-        console.log('Error from BrowseCandidates');
+        console.log('Error from BrowseCandidates' + err);
       });
   }, []);
 
   const candidateList =
     candidates.length === 0
       ? <Text>There are no candidates!</Text>
-      : candidates.map((candidate) => <CandidateBox onPress={() => { navigation.navigate('ViewCandidate', candidate) }} firstName={candidate.firstName} lastName={candidate.lastName}
+      : candidates.sort((a, b) => {
+        if (a.starred === b.starred) {
+          return a.lastName < b.lastName ? -1 : 1;
+        }
+        return b.starred - a.starred;
+      }).map((candidate) => <CandidateBox onPress={() => { navigation.navigate('ViewCandidate', candidate) }} firstName={candidate.firstName} lastName={candidate.lastName}
         major={candidate.major} degree={candidate.degree} starred={candidate.starred} key={candidate._id}/>);
 
   return (
