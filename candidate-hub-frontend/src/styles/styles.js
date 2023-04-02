f09ce58f290f8a6fd0190d2c5c2b13299d2dbdab
ardymarky <aqmarkin@gmail.com>
2023-04-02 01:37:19 -0500

    css stuff

--- candidate-hub-frontend/src/styles/styles.js
+++ candidate-hub-frontend/src/styles/styles.js
@@ -23,15 +23,17 @@ const styles = StyleSheet.create({
     candidateBox: {
         borderStyle: 'solid',
         borderWidth: '2px',
-        borderColor: '#fff',
+        borderColor: '#d4dcfc',
         borderRadius: '8px',
         marginTop: 10,
-        padding: 10,
+        marginHorizontal: 7.5,
+        padding: 15,
         minWidth: '90%',
     },
     candidateBoxText: {
-        fontSize: 16,
-        color: '#fff'
+        fontSize: 20,
+        fontWeight: '400',
+        color: Colors.HEADER_COLOR
     },
     inputField: {
         backgroundColor: Colors.InputField,
@@ -55,6 +57,19 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1
+    },
+    star: {
+        color: Colors.HEADER_COLOR,
+        alignSelf: 'flex-end',
+        bottom: 20,
+        right: 20,
+        position: 'absolute',
+    },
+    star2: {
+        color: Colors.HEADER_COLOR,
+        position: 'absolute',
+        bottom:305,
+        right: 20
     },
     textInput: {
           borderColor: Colors.InputField,
