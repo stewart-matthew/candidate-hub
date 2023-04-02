f91f7b0f7d16233604150ef4a2ac9fbe5faa5179
Sam Gaines <samuelmgaines@gmail.com>
2023-04-02 02:09:28 +0100

    Got some candies

--- candidate-hub-frontend/src/screens/BrowseCandidates.js
+++ candidate-hub-frontend/src/screens/BrowseCandidates.js
@@ -1,45 +1,34 @@
 import { ActivityIndicator, Text, View } from 'react-native';
 import Button from '../components/Button';
+import axios from 'axios'
 import { baseURL } from '../config/config/';
 import { useState, useEffect } from 'react';
 import { FlatList } from 'react-native-gesture-handler';
 
 const BrowseCandidatesScreen = ({ navigation }) => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  const getCandidates = async () => {
-    try {
-      const response = await fetch("http://10.122.109.151:8082/api/candidates");
-      const json = await response.json();
-      setData(json);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const [candidates, setCandidates] = useState([]);
 
   useEffect(() => {
-    getCandidates();
+    axios
+      .get(baseURL)
+      .then((res) => {
+        setCandidates(res.data);
+      })
+      .catch((err) => {
+        console.log('Error from BrowseCandidates');
+      });
   }, []);
 
-  console.log(data);
+  const candidateList =
+    candidates.length === 0
+      ? 'there is no candidate!'
+      : candidates.map((candidate) => <Button title={candidate.firstName} key={candidate._id}/>);
+
   return (
-    <View style={{flex: 1, padding: 24}}>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <FlatList
-          data={data}
-          keyExtractor={candidate => candidate._id}
-          renderItem={(candidate) => (
-            <Button title={candidate.firstName}/>
-          )}
-        />
-      )}
+    <View>
+      <Text>{candidateList}</Text>
     </View>
   );
-};
+}
 
 export default BrowseCandidatesScreen;
