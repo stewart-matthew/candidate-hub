a63d1b1a07d42c8e05fe8a136348189bf48c7324
Sam Gaines <samuelmgaines@gmail.com>
2023-04-02 01:28:12 +0100

    .

---
+++ candidate-hub-frontend/src/config/config.js
@@ -0,0 +1,2 @@
+const ip = "10.122.109.151";
+export const baseURL = "http://" + ip + ":8082/api/candidates";
--- candidate-hub-frontend/src/screens/BrowseCandidates.js
+++ candidate-hub-frontend/src/screens/BrowseCandidates.js
@@ -1,15 +1,45 @@
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
 import Button from '../components/Button';
+import { baseURL } from '../config/config/';
+import { useState, useEffect } from 'react';
+import { FlatList } from 'react-native-gesture-handler';
 
 const BrowseCandidatesScreen = ({ navigation }) => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getCandidates = async () => {
+    try {
+      const response = await fetch("http://10.122.109.151:8082/api/candidates");
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getCandidates();
+  }, []);
+
+  console.log(data);
   return (
-    <View>
-      <Button
-          onPress={() => { alert('balls');/*navigation.navigate('AddCandidate')*/ }}
-          title={"Get those candidates"}
+    <View style={{flex: 1, padding: 24}}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={candidate => candidate._id}
+          renderItem={(candidate) => (
+            <Button title={candidate.firstName}/>
+          )}
         />
+      )}
     </View>
-  )
-}
+  );
+};
 
 export default BrowseCandidatesScreen;
